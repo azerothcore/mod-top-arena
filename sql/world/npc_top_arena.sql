e148013707bae53b0223c8c9fb9e65093a5a8e8b
devovh <devovh@gmail.com>
2022-05-14 14:10:57 +0200

    * Fix Mod Arena Top.

--- sql/world/npc_top_arena.sql
+++ sql/world/npc_top_arena.sql
@@ -1,1 +1,29 @@
-INSERT INTO `creature_template` VALUES (55333, 0, 0, 0, 0, 0, 31833, 0, 0, 0, 'Top Arena Ranking', 'WowTorn', NULL, 0, 80, 80, 0, 35, 1, 1, 1.14286, 1, 1, 0, 0, 0, 0, 1, 2000, 2000, 2, 0, 2048, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 50, 50, 1, 0, 0, 1, 0, 0, 'arenatop', 12340);
+-- --------------------------------------------------------------------------------------
+--	ARENA TOP - 55333
+-- --------------------------------------------------------------------------------------
+SET
+@Entry 		:= 55333,
+@Model 		:= 31833, -- Goblin Banker
+@Name 		:= "Destroyer",
+@Title 		:= "Top Arena Ranking",
+@Icon 		:= "LootAll",
+@GossipMenu := 0,
+@MinLevel 	:= 80,
+@MaxLevel 	:= 80,
+@Faction 	:= 35,
+@NPCFlag 	:= 3,
+@Scale		:= 1.0,
+@Rank		:= 0,
+@Type 		:= 7,
+@TypeFlags 	:= 0,
+@FlagsExtra := 2,
+@AIName		:= "SmartAI",
+@Script 	:= "arenatop";
+
+-- NPC
+DELETE FROM creature_template WHERE entry = @Entry;
+INSERT INTO creature_template (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`) VALUES
+(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 1, @FlagsExtra, @AIName, @Script);
+
+DELETE FROM `npc_text` WHERE `ID`=@Entry;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry, 'Hey there, the name\'s Skinny. You feelin\' lucky?');
