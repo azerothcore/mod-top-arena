532b07f6e59297dca7234deabf9b84592d9be123
devovh <devovh@gmail.com>
2024-03-10 13:21:42 +0100

    * Fix Mod Arena Top. (#3)
    * * Fix Mod Arena Top.
    
    * * Rename Model NPC Comment.

--- src/Top_Arena_NPC.cpp
+++ src/Top_Arena_NPC.cpp
@@ -1,6 +1,6 @@
 
 
-#include "PrecompiledHeaders/ScriptPCH.h"
+#include "ScriptMgr.h"
 #include <cstring>
 #include <string.h>
 #include "ObjectMgr.h"
@@ -10,6 +10,7 @@
 #include "World.h"
 #include "Player.h"
 #include "Chat.h"
+#include "ScriptedGossip.h"
 
 enum ArenaRankActionIds {
     ARENA_2V2_LADDER = GOSSIP_ACTION_INFO_DEF + 1,
@@ -90,13 +91,15 @@ class Top_Arena_NPC : public CreatureScript
             }
             return Class_s;
         }
-        
-        std::string getPlayerStatus(uint32 guid) {
-            Player *player = sObjectAccessor->FindPlayer(guid);
+
+        //TO DO
+
+        /*std::string getPlayerStatus(ObjectGuid guid) {
+            Player *player = ObjectAccessor::FindPlayer(guid);
             if(!player)
                 return "Offline";
             return "Online";
-        }
+        }*/
         
         std::string getWinPercent(uint32 wins, uint32 losses) {
             uint32 totalGames = wins + losses;
@@ -113,11 +116,11 @@ class Top_Arena_NPC : public CreatureScript
         Top_Arena_NPC() : CreatureScript("arenatop"){}
         
         bool OnGossipHello(Player *player, Creature *creature) {
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Top PvP 1v1.", GOSSIP_SENDER_MAIN, ARENA_5V5_LADDER);
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Top PvP 2v2.", GOSSIP_SENDER_MAIN, ARENA_2V2_LADDER);
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Top PvP 3v3.", GOSSIP_SENDER_MAIN, ARENA_3V3_LADDER);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "Top PvP 2v2.", GOSSIP_SENDER_MAIN, ARENA_2V2_LADDER);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "Top PvP 3v3.", GOSSIP_SENDER_MAIN, ARENA_3V3_LADDER);
+            AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "Top PvP 5v5.", GOSSIP_SENDER_MAIN, ARENA_5V5_LADDER);
             
-            player->SEND_GOSSIP_MENU(ARENA_GOSSIP_HELLO, creature->GetGUID());
+            SendGossipMenuFor(player, ARENA_GOSSIP_HELLO, creature->GetGUID());
             
             return true;
         }
@@ -132,39 +135,39 @@ class Top_Arena_NPC : public CreatureScript
                     break;
                 }
                 case ARENA_2V2_LADDER:
-                case ARENA_5V5_LADDER:
                 case ARENA_3V3_LADDER:
+                case ARENA_5V5_LADDER:
                 {
                     uint32 teamType = optionToTeamType(uiAction);
-                    QueryResult result = CharacterDatabase.PQuery(
+                    QueryResult result = CharacterDatabase.Query(
                             "SELECT arenaTeamId, name, rating, seasonWins, seasonGames - seasonWins "
-                            "FROM arena_team WHERE type = '%u' ORDER BY rating DESC LIMIT %u;", teamType, ARENA_MAX_RESULTS
+                            "FROM arena_team WHERE type = '{}' ORDER BY rating DESC LIMIT {};", teamType, ARENA_MAX_RESULTS
                             );
                     
                     if(!result) {
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, "Farewell.", GOSSIP_SENDER_MAIN, ARENA_GOODBYE);
-                        player->SEND_GOSSIP_MENU(ARENA_GOSSIP_NOTEAMS, creature->GetGUID());
+                        AddGossipItemFor(player, GOSSIP_ICON_TALK, "Farewell.", GOSSIP_SENDER_MAIN, ARENA_GOODBYE);
+                        SendGossipMenuFor(player, ARENA_GOSSIP_NOTEAMS, creature->GetGUID());
                     } else {
                         //uint64 rowCount = result->GetRowCount();
                         std::string name;
                         uint32 teamId, rating, seasonWins, seasonLosses, rank = 1;
                         do {
                             Field *fields = result->Fetch();
-                            teamId = fields[0].GetUInt32();
-                            name = fields[1].GetString();
-                            rating = fields[2].GetUInt32();
-                            seasonWins = fields[3].GetUInt32();
-                            seasonLosses = fields[4].GetUInt32();
+                            teamId = fields[0].Get<uint32>();
+                            name = fields[1].Get<std::string>();
+                            rating = fields[2].Get<uint32>();
+                            seasonWins = fields[3].Get<uint32>();
+                            seasonLosses = fields[4].Get<uint32>();
                             
                             std::stringstream buffer;
                             buffer << rank << ". [" << rating << "] " << name;
                             buffer << " (" << seasonWins << "-" << seasonLosses << ")";
-                            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, buffer.str(), GOSSIP_SENDER_MAIN, ARENA_START_TEAM_LOOKUP + teamId);
+                            AddGossipItemFor(player, GOSSIP_ICON_BATTLE, buffer.str(), GOSSIP_SENDER_MAIN, ARENA_START_TEAM_LOOKUP + teamId);
                             
                             rank++;
                         } while(result->NextRow());
                         
-                        player->SEND_GOSSIP_MENU(ARENA_GOSSIP_TEAMS, creature->GetGUID());
+                        SendGossipMenuFor(player, ARENA_GOSSIP_TEAMS, creature->GetGUID());
                     }
                     break;
                 }
@@ -175,31 +178,40 @@ class Top_Arena_NPC : public CreatureScript
                         uint32 teamId = uiAction - ARENA_START_TEAM_LOOKUP;
                         
                         // lookup team
-                        QueryResult result = CharacterDatabase.PQuery(
-                            //      0     1       2           3
-                            "SELECT name, rating, seasonWins, seasonGames - seasonWins, "
-                            //  4      5                     6     7             8
-                            "weekWins, weekGames - weekWins, rank, captainGuid , type "
-                            "FROM arena_team WHERE arenaTeamId = '%u'", teamId);
-                        
+                        QueryResult result = CharacterDatabase.Query("SELECT "
+                            "`name`,"                 // 0
+                            "`rating`,"  // 1
+                            "`seasonWins`,"   // 2
+                            "`seasonGames`"   // 3
+                            " - " // 3
+                            "`seasonWins`," // 3
+                            "`weekWins`,"  // 4
+                            "`weekGames`"   // 5
+                            " - " // 5
+                            "`weekWins`,"  // 5
+                            "`rank`,"   // 6
+                            "`captainGuid`,"   // 7
+                            "`type` "   // 8
+                            "FROM `arena_team` WHERE `arenaTeamId` = '{}'", teamId);
+
                         // no team found
                         if(!result) {
-                            player->GetSession()->SendNotification("Arena team not found...");
+                            SendNotification(player->GetSession(), "Arena team not found...");
                             player->PlayerTalkClass->SendCloseGossip();
                             return true;
                         }
                         
                         // populate the results
                         Field *fields = result->Fetch();
-                        std::string name = fields[0].GetString();
-                        uint32 rating = fields[1].GetUInt32();
-                        uint32 seasonWins = fields[2].GetUInt32();
-                        uint32 seasonLosses = fields[3].GetUInt32();
-                        uint32 weekWins = fields[4].GetUInt32();
-                        uint32 weekLosses = fields[5].GetUInt32();
-                        uint32 rank = fields[6].GetUInt32();
-                        uint32 captainGuid = fields[7].GetUInt32();
-                        uint32 type = fields[8].GetUInt32();
+                        std::string name = fields[0].Get<std::string>();
+                        uint32 rating = fields[1].Get<uint32>();
+                        uint32 seasonWins = fields[2].Get<uint32>();
+                        uint32 seasonLosses = fields[3].Get<uint32>();
+                        uint32 weekWins = fields[4].Get<uint32>();
+                        uint32 weekLosses = fields[5].Get<uint32>();
+                        uint32 rank = fields[6].Get<uint32>();
+                        uint32 captainGuid = fields[7].Get<uint32>();
+                        uint32 type = fields[8].Get<uint32>();
                         uint32 parentOption = teamTypeToOption(type);
                         
                         std::string seasonWinPercentage = getWinPercent(seasonWins, seasonLosses);
@@ -207,20 +219,20 @@ class Top_Arena_NPC : public CreatureScript
                         
                         std::stringstream buf;
                         buf << "Team Name: " << name;
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                        AddGossipItemFor(player, GOSSIP_ICON_BATTLE, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                         buf.str("");
                         buf << "Rating: " << rating << " (rank " << rank << ", bracket " << type << "v" << type << ")";
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                        AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                         buf.str("");
                         buf << "Total Week: " << weekWins << "-" << weekLosses << " (" << weekWinPercentage << " win), " << (weekWins + weekLosses) << " played"; 
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                        AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                         buf.str("");
                         buf << "Total Season: " << seasonWins << "-" << seasonLosses << " (" << seasonWinPercentage << " win), " << (seasonWins + seasonLosses) << " played"; 
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                        AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                         
-                        QueryResult members = CharacterDatabase.PQuery( "SELECT  a.guid, a.personalRating, a.weekWins, a.weekGames - a.weekWins, a.seasonWins, a.seasonGames - a.seasonWins, c.name, c.race, c.class, c.level FROM arena_team_member a LEFT JOIN characters c ON c.guid = a.guid WHERE arenaTeamId = '%u' ORDER BY a.guid = '%u' DESC, a.seasonGames DESC, c.name ASC", teamId, captainGuid);
+                        QueryResult members = CharacterDatabase.Query( "SELECT  a.guid, a.personalRating, a.weekWins, a.weekGames - a.weekWins, a.seasonWins, a.seasonGames - a.seasonWins, c.name, c.race, c.class, c.level FROM arena_team_member a LEFT JOIN characters c ON c.guid = a.guid WHERE arenaTeamId = '{}' ORDER BY a.guid = '{}' DESC, a.seasonGames DESC, c.name ASC", teamId, captainGuid);
                         if(!members) {
-                            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "No team members found...?", GOSSIP_SENDER_MAIN, parentOption);
+                            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "No team members found...?", GOSSIP_SENDER_MAIN, parentOption);
                         } else {
                             uint32 memberPos = 1;
                             uint32 memberCount = members->GetRowCount();
@@ -229,21 +241,21 @@ class Top_Arena_NPC : public CreatureScript
                             
                             buf.str("");
                             buf << memberCount << " team " << ((memberCount == 1) ? "member" : " members") << " found:";
-                            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                            AddGossipItemFor(player, GOSSIP_ICON_CHAT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                             
                             do {
                                 // populate fields
                                 fields = members->Fetch();
-                                guid = fields[0].GetUInt32();
-                                personalRating = fields[1].GetUInt32();
-                                weekWins= fields[2].GetUInt32();
-                                weekLosses = fields[3].GetUInt32();
-                                seasonWins = fields[4].GetUInt32();
-                                seasonLosses = fields[5].GetUInt32();
-                                name = fields[6].GetString();
-                                race = raceToString(fields[7].GetUInt8());
-                                Class = classToString(fields[8].GetUInt8());
-                                level = fields[9].GetUInt32();
+                                guid = fields[0].Get<uint32>();
+                                personalRating = fields[1].Get<uint32>();
+                                weekWins= fields[2].Get<uint32>();
+                                weekLosses = fields[3].Get<uint32>();
+                                seasonWins = fields[4].Get<uint32>();
+                                seasonLosses = fields[5].Get<uint32>();
+                                name = fields[6].Get<std::string>();
+                                race = raceToString(fields[7].Get<uint8>());
+                                Class = classToString(fields[8].Get<uint8>());
+                                level = fields[9].Get<uint32>();
                                 
                                 seasonWinPercentage = getWinPercent(seasonWins, seasonLosses);
                                 weekWinPercentage = getWinPercent(weekWins, weekLosses);
@@ -253,18 +265,18 @@ class Top_Arena_NPC : public CreatureScript
                                 buf << memberPos << ". "; 
                                 if (guid == captainGuid) 
                                     buf <<  "Team Captain ";
-                                buf << name << ", " << getPlayerStatus(guid);
+                                buf << name; //<< ", " << getPlayerStatus(guid); TO DO
                                 
-                                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                                 buf.str("");
                                 buf << "Level " << level << " " << race << " " << Class << ", " << personalRating << " personal rating.";
-                                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                                AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                                 buf.str("");
                                 buf << "Week: " << weekWins << "-" << weekLosses << " (" << weekWinPercentage << " win), " << (weekWins + weekLosses) << " played"; 
-                                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                                AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                                 buf.str("");
                                 buf << "Season: " << seasonWins << "-" << seasonLosses << " (" << seasonWinPercentage << " win), " << (seasonWins + seasonLosses) << " played"; 
-                                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                                AddGossipItemFor(player, GOSSIP_ICON_DOT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
                                 memberPos++;
                             } while(members->NextRow());
                             
@@ -272,8 +284,8 @@ class Top_Arena_NPC : public CreatureScript
                         
                         buf.str("");
                         buf << "Go back to " << type << "v" << type << " ladder!";
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
-                        player->SEND_GOSSIP_MENU(ARENA_GOSSIP_TEAM_LOOKUP, creature->GetGUID());
+                        AddGossipItemFor(player, GOSSIP_ICON_CHAT, buf.str(), GOSSIP_SENDER_MAIN, parentOption);
+                        SendGossipMenuFor(player, ARENA_GOSSIP_TEAM_LOOKUP, creature->GetGUID());
                     }
                 }
             }
